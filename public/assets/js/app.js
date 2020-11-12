0dbd6431bbd5684feaa48f28d1828494aa135d04
Temitope Ogungbesan <temitope.ogungbesan.5@gmail.com>
2020-11-12 18:06:34 +0100

    sending data to firebase

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,19 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBDugoQX3XTgl32seFEtimxJ73yC38MTp0",
+    authDomain: "stem-girls-307c9.firebaseapp.com",
+    databaseURL: "https://stem-girls-307c9.firebaseio.com",
+    projectId: "stem-girls-307c9",
+    storageBucket: "stem-girls-307c9.appspot.com",
+    messagingSenderId: "231056994218",
+    appId: "1:231056994218:web:3989b1a9a6dfa68df785d3",
+    measurementId: "G-9Y900LVBMT"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
   //Reference for form collection(3)
   let formMessage = firebase.database().ref('mentor');
 
@@ -27,7 +43,7 @@ function submitMentor(e) {
    //Form Reset After Submission
    document.getElementById('mentorform').reset();
 
-
+}
 
 //Send Message to Firebase(4)
 function sendMentor(name, email, phone, location, age) {
@@ -39,5 +55,4 @@ function sendMentor(name, email, phone, location, age) {
       location: location,
       age: age
     });
-}
 }
